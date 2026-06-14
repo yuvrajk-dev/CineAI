78b7b9ec97e1fd7cd7c84e9126a7ffde7bb5f5a1
Yuvraj Kumar <yuvrajk.dev@gmail.com>
2026-06-14 23:09:14 +0530

    Add Auth component as a child route in the appRouter

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from "react-router";
 import Home from "./Home";
+import Auth from "../pages/Auth";
 
 const appRouter = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+    children: [
+      {
+        index: true,
+        element: <Auth />,
+      },
+    ],
   },
   {},
 ]);
