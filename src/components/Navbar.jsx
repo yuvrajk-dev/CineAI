c777caab0f8ee58ee8aa807cf305866cc8052c92
Yuvraj Kumar <yuvrajk.dev@gmail.com>
2026-06-14 23:09:23 +0530

    Add Navbar component for site navigation

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+    <nav className="w-full absolute h-20 px-6 md:px-20 flex items-center  from-black bg-linear-to-b ">
+      <h1 className="text-4xl md:text-5xl font-bold">
+        <span className="text-(--primary)">Cine</span>
+        <span className="text-white">AI</span>
+      </h1>
+    </nav>
+  );
+};
+
+export default Navbar;
