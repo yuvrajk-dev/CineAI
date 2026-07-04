501c232b9a2e22d591bd7b520fdff92b40f8643f
Yuvraj Kumar <yuvrajk.dev@gmail.com>
2026-07-04 19:25:46 +0530

    Add custom hooks for fetching user data and now playing movies

---
+++ src/hooks/useFetchUser.js
@@ -0,0 +1,37 @@
+import { useEffect } from "react";
+import supabase from "../utils/supabase";
+
+const useFetchUser = (setData, setLoading) => {
+  const fetchUser = async () => {
+    try {
+      const { data, error } = await supabase.auth.getUser();
+      if (error) throw error;
+      const user = data.user;
+
+      if (!user) return;
+      const { data: profile, error: profileError } = await supabase
+        .from("profiles")
+        .select("username")
+        .eq("id", user.id)
+        .single();
+
+      if (profileError) throw profileError;
+
+      setData({
+        id: user.id,
+        email: user.email,
+        username: profile.username,
+      });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+};
+
+export default useFetchUser;
