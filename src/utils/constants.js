194dd3f0286f2f04a939c4aa6cec200f7541d177
Yuvraj Kumar <yuvrajk.dev@gmail.com>
2026-07-06 16:17:58 +0530

    Add IMG_CDN_URL constant for image resource management

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -6,3 +6,5 @@ export const API_OPTIONS = {
       "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNTE5OThiZGU1MGY0ZjMwMDg5ZmU5Njk4NjhhNGQyYyIsIm5iZiI6MTc4Mjk4ODAyMC4zMjUsInN1YiI6IjZhNDYzY2Y0NzU4NmNjZDg4MmYxYzg1MyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.pMo0uTPWqavtanLSInioaQoDEkgrRRYNLURhv6SRtHE",
   },
 };
+
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500";
